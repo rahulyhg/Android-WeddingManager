f3c16081f9b44ff83487e8f7e395e593fcde86d8
SOUHEIL <souheildevelop@gmail.com>
2014-03-23 00:19:00 +0100

    Test XML Parser-Organize Code
    Remove unnecessary code, organize imports

--- gen/com/sim/weddingmanager/R.java
+++ gen/com/sim/weddingmanager/R.java
@@ -8,20 +8,6 @@
 package com.sim.weddingmanager;
 
 public final class R {
-    public static final class array {
-        public static final int cake_image=0x7f050005;
-        public static final int cake_name=0x7f050003;
-        public static final int cake_price=0x7f050004;
-        public static final int car_image=0x7f050008;
-        public static final int car_name=0x7f050006;
-        public static final int car_price=0x7f050007;
-        public static final int deco_image=0x7f05000b;
-        public static final int deco_name=0x7f050009;
-        public static final int deco_price=0x7f05000a;
-        public static final int hotel_image=0x7f050002;
-        public static final int hotel_name=0x7f050000;
-        public static final int hotel_price=0x7f050001;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -51,29 +37,29 @@ public final class R {
         public static final int img_takacim=0x7f020017;
     }
     public static final class id {
-        public static final int button_validate=0x7f080012;
-        public static final int cake_image=0x7f08000d;
-        public static final int cake_price=0x7f080005;
-        public static final int cake_selection=0x7f080004;
-        public static final int car_image=0x7f08000e;
-        public static final int car_price=0x7f080003;
-        public static final int car_selection=0x7f080002;
-        public static final int deco_image=0x7f08000c;
-        public static final int deco_price=0x7f080007;
-        public static final int deco_selection=0x7f080006;
-        public static final int edit_date=0x7f080011;
-        public static final int edit_name=0x7f080010;
-        public static final int hotel_image=0x7f08000b;
-        public static final int hotel_price=0x7f080001;
-        public static final int hotel_selection=0x7f080000;
-        public static final int iconNew=0x7f080013;
-        public static final int lblName=0x7f080014;
-        public static final int lblPrice=0x7f080015;
-        public static final int linear1=0x7f08000a;
-        public static final int lista=0x7f08000f;
-        public static final int menu_calculate=0x7f080016;
-        public static final int total=0x7f080008;
-        public static final int welcome_text=0x7f080009;
+        public static final int button_validate=0x7f070012;
+        public static final int cake_image=0x7f07000d;
+        public static final int cake_price=0x7f070005;
+        public static final int cake_selection=0x7f070004;
+        public static final int car_image=0x7f07000e;
+        public static final int car_price=0x7f070003;
+        public static final int car_selection=0x7f070002;
+        public static final int deco_image=0x7f07000c;
+        public static final int deco_price=0x7f070007;
+        public static final int deco_selection=0x7f070006;
+        public static final int edit_date=0x7f070011;
+        public static final int edit_name=0x7f070010;
+        public static final int hotel_image=0x7f07000b;
+        public static final int hotel_price=0x7f070001;
+        public static final int hotel_selection=0x7f070000;
+        public static final int iconNew=0x7f070013;
+        public static final int lblName=0x7f070014;
+        public static final int lblPrice=0x7f070015;
+        public static final int linear1=0x7f07000a;
+        public static final int lista=0x7f07000f;
+        public static final int menu_calculate=0x7f070016;
+        public static final int total=0x7f070008;
+        public static final int welcome_text=0x7f070009;
     }
     public static final class layout {
         public static final int activity_billing=0x7f030000;
@@ -83,7 +69,7 @@ public final class R {
         public static final int one_event=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_dashboard=0x7f070000;
+        public static final int activity_dashboard=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -114,10 +100,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
